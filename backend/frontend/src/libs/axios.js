eb909063e314c1b9142742aa60dd4f5bc255f741
wanZzz <1343837706@qq.com>
2019-05-30 16:15:35 +0800

    修复django无法接收axios的post参数

--- backend/frontend/src/libs/axios.js
+++ backend/frontend/src/libs/axios.js
@@ -2,7 +2,11 @@ import axios from 'axios'
 import store from '@/store'
 // import { Spin } from 'iview'
 const addErrorLog = errorInfo => {
-  const { statusText, status, request: { responseURL } } = errorInfo
+  const {
+    statusText,
+    status,
+    request: { responseURL }
+  } = errorInfo
   let info = {
     type: 'ajax',
     code: status,
@@ -75,15 +79,32 @@ class HttpRequest {
       }
     )
   }
-
+  transform (data) {
+    let param = new URLSearchParams()
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        param.append(key, data[key])
+      }
+    }
+    return param
+  }
   // const axiosUrl = env === 'development'
   // ? 'http://localhost:8080/'
   // : 'http://192.168.0.1:8080'
 
   request (options) {
+    // axios 默认是不发送cookie
+    axios.defaults.withCredentials = true
+
     const instance = axios.create()
     options = Object.assign(this.getInsideConfig(), options)
     this.interceptors(instance, options.url)
+    if(options.params){
+      options.params = this.transform(options.params)
+    }
+    if(options.data){
+      options.data = this.transform(options.data)
+    }
     return instance(options)
   }
 }
