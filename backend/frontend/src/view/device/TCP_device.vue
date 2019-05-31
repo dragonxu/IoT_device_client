1ff75e93a7403c05f8ca30aff962ea091576296d
wanZzz <1343837706@qq.com>
2019-05-31 16:33:19 +0800

    添加TCP设备前端配置页面，并js合法性校验

---
+++ backend/frontend/src/view/device/TCP_device.vue
@@ -0,0 +1,105 @@
+<template>
+        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
+            <FormItem label="设备名" prop="name">
+                <Input v-model="formValidate.name" placeholder="设备名必须唯一" style="width: 50%"></Input>
+            </FormItem>
+            <FormItem label="描述" prop="description">
+                <Input type="textarea" :rows="4" v-model="formValidate.description" style="width: 50%" placeholder="请输入设备描述"></Input>
+            </FormItem>
+            <FormItem label="设备地址" prop="slave">
+                    <Input v-model="formValidate.slave" number placeholder="可输入1-255之间的整数值" style="width: 50%" ></Input>
+                </FormItem>
+            <FormItem label="ip地址" prop="ip">
+                <Input v-model="formValidate.ip" placeholder="请输入正确的IP地址：例如：192.168.1.10" style="width: 50%"></Input>
+            </FormItem>
+            <FormItem label="端口号" prop="port" placeholder="例如：502"  >
+                <Input v-model="formValidate.port" number style="width: 50%"></Input>
+            </FormItem>
+            <FormItem>
+                <Button type="primary" @click="handleSubmit('formValidate')">确认</Button>
+                <Button @click="handleReset('formValidate')" style="margin-left: 8px">重置</Button>
+            </FormItem>
+        </Form>
+    </template>
+    <script>
+        export default {
+            name: 'TCP_device',
+            data () {
+                // 注意写在return上方
+                const checkIP= (rule, value, callback) =>{
+                    let ipRegex =  /(2(5[0-5]{1}|[0-4]\d{1})|[0-1]?\d{1,2})(\.(2(5[0-5]{1}|[0-4]\d{1})|[0-1]?\d{1,2})){3}/g
+                    if (value === '') {
+                        callback(new Error('Please enter your ip'))
+                    }
+                    else if(!ipRegex.test(value)){
+                        callback(new Error('Please enter correct ip'))
+                    }
+                    else{
+                        callback();
+                    }
+                };
+                const checkSlave = (rule, value, callback) =>{
+                    // 转为整形
+                    value = value -0
+                    if (!value)
+                        callback(new Error('please enter correct address(1-255)'))
+                    if (value < 0 || value > 255)
+                        callback(new Error('please enter correct address(1-255)'))
+                    else
+                        callback();
+                };
+                const checkPort = (rule, value, callback) => {
+                    value = value-0
+                    if(!value){
+                        callback(new Error('please enter correct port(1-65535)'))
+                    }
+                    else if(value < 0 || value > 65535)
+                        callback(new Error('please enter correct port(1-65535)'))
+                    else
+                        callback();
+                };
+                return {
+                    formValidate: {
+                        name: '',
+                        description: '',
+                        ip: '127.0.0.1',
+                        port: null,
+                        slave: '',
+                    },
+                    // 写在return中
+                    ruleValidate: {
+                        name: [
+                            { required: true, message: 'The name cannot be empty', trigger: 'blur' }
+                        ],
+                        description: [
+                            { required: false, message: 'Mailbox cannot be empty', trigger: 'blur' },
+                        ],
+                        ip: [
+                            { required: true, validator: checkIP, trigger: 'blur' }
+                        ],
+                        port: [
+                            { required: true, validator: checkPort, trigger: 'blur' }
+                        ],
+                        slave: [
+                            { required: true, validator: checkSlave, trigger: 'blur' }
+                        ],
+                    }
+                }
+            },
+            methods: {
+                handleSubmit (name) {
+                    this.$refs[name].validate((valid) => {
+                        if (valid) {
+                            this.$Message.success('Success!');
+                        } else {
+                            this.$Message.error('Fail!');
+                        }
+                    })
+                },
+                handleReset (name) {
+                    this.$refs[name].resetFields();
+                }
+            }
+        }
+    </script>
+    
---
+++ backend/frontend/src/view/device/device.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>网关：{{ gateway_name }}</h1>
+    <Divider style="padding: 0%"></Divider>
+    <Button type="primary" icon="md-add-circle" @click='create_tcp'>添加TCP设备</Button>
+    <Button  icon="md-add" @click='create_rtu'>添加RTU设备</Button>
+    <Divider></Divider>
+    <tcp-device v-if="show_view=='tcp'"></tcp-device>
+    <rtu-device v-if="show_view=='rtu'"></rtu-device>
+  </div>
+</template>
+
+<script>
+  import RTU_device from './RTU_device.vue'
+  import TCP_device from './TCP_device.vue'
+  export default {
+    data() {
+      return {
+        show_view: '',
+        gateway_name: ''
+      }
+    },
+    components:{
+        'tcp-device': TCP_device,
+        'rtu-device': RTU_device
+    },
+    methods: {
+        create_tcp(){
+            this.show_view ='tcp'
+
+        },
+        create_rtu(){
+            this.show_view = 'rtu'
+        }
+    },
+    mounted() {
+      // 获取路由参数
+      this.gateway_name = this.$route.params.gateway_name
+    }
+  }
+</script>
